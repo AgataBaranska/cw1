62f0e42f302d6260ef63c493e0a5caf799008750
s19487 <s19487@pjwstk.edu.pl>
2020-11-07 23:11:39 +0100

    dodatkowe-wymagania

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -11,22 +14,51 @@ namespace Cw1
         public static async Task Main(string[] args)
         {
 
-            var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync("https://www.pja.edu.pl");
-            string html = "";
-
-            if (response.IsSuccessStatusCode)
+            if (args.Length == 0 || args == null)
             {
-                html = await response.Content.ReadAsStringAsync();
-                var regex = new Regex("[a-z0-9]+@[a-z.]+");
-                MatchCollection matches = regex.Matches(html);
-                foreach (var i in matches)
-                {
-                    Console.WriteLine(i);
-                }
+                throw new ArgumentNullException("The url parameter was not passed");
+            }
+            bool result = Uri.TryCreate(args[0], UriKind.Absolute, out Uri uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+           
+            if (!result)
+            {
+                throw new ArgumentException("url is not correct");
+            }
+
+            using HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(uriResult);
+
+
+            string content = "";
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                content = await response.Content.ReadAsStringAsync();
 
             }
-            Console.WriteLine("Koniec");
+            else
+            {
+                Console.WriteLine("Bład w czasie pobierania strony");
+            }
+
+            var regex = new Regex("[a-z0-9]+@[a-z.]+");
+            MatchCollection matches = regex.Matches(content);
+            if (matches.Count == 0)
+            {
+                Console.WriteLine("Nie znaleziono adresów email");
+            }
+            else
+            {
+                HashSet<string> uniqueEmail = new HashSet<string>();
+                foreach (Match m in matches)
+                {
+                    uniqueEmail.Add(m.Value);
+                }
+                uniqueEmail.ToList().ForEach(Console.WriteLine);
+            }
+
         }
+
     }
 }
+
